1ba37a4f7ba8d72aee4d0dc48fd6014911439ce6
Bruno Mario Veronesi <endeios@gmail.com>
2023-02-25 08:15:05 +0100

    first test

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,18 @@
 pub struct Game {
 
 }
+
+impl Game {
+    pub(crate) fn score(&self) -> i32 {
+        0
+    }
+}
+
+impl Game {
+    pub(crate) fn roll(&self, number_of_pins: i32) {
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::Game;
@@ -9,6 +21,14 @@ mod tests {
     #[test]
     fn game_exists() {
         let game: Game = Game{};
+    }
+
+    #[test]
+    fn game_supports_gutter_game(){
+        let game: Game = Game{};
+        game.roll(0);
+        assert_eq!(game.score(), 0);
+
     }
 
 }
