5e82864a43e9b137150c282564a3c5b06b36b9a0
Bruno Mario Veronesi <endeios@gmail.com>
2023-02-25 10:31:53 +0100

    second test

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
+#[allow(unused_variables)]
 
-pub struct Game {
-
-}
+pub struct Game {}
 
 impl Game {
     pub(crate) fn score(&self) -> i32 {
@@ -10,8 +9,7 @@ impl Game {
 }
 
 impl Game {
-    pub(crate) fn roll(&self, number_of_pins: i32) {
-    }
+    pub(crate) fn roll(&self, number_of_pins: i32) {}
 }
 
 #[cfg(test)]
@@ -20,15 +18,24 @@ mod tests {
 
     #[test]
     fn game_exists() {
-        let game: Game = Game{};
+        let game: Game = Game {};
     }
 
     #[test]
-    fn game_supports_gutter_game(){
-        let game: Game = Game{};
-        game.roll(0);
+    fn game_supports_gutter_game() {
+        let game: Game = Game {};
+        for _ in 0..20 {
+            game.roll(0);
+        }
         assert_eq!(game.score(), 0);
-
     }
 
+    #[test]
+    fn game_supports_all_one_game() {
+        let game: Game = Game {};
+        for _ in 0..20 {
+            game.roll(1);
+        }
+        assert_eq!(game.score(), 20);
+    }
 }
